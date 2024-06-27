a2ddd970aa6a73cdbb5f9582ea8d1210c657a4aa
Adarsh <adarsh9540984202@gmail.com>
2024-06-27 20:51:11 +0530

    complete responsive

--- src/components/FullBlog.tsx
+++ src/components/FullBlog.tsx
@@ -6,35 +6,45 @@ export const FullBlog = ({ blog }: { blog: Blog }) => {
   return (
     <div>
       <Navbar />
-      <div className="grid grid-cols-12 p-8 pt-200">
+      <div className="lg:grid lg:grid-cols-12 p-8 pt-6">
         <div className="col-span-8">
-          <div className="text-5xl font-extrabold">
+          <div className="text-2xl lg:text-5xl font-extrabold">
             {blog.title}
           </div>
-          <div className="text-slate-500 pt-2" >
-            Post on 22nd june 2024
+          <div className="flex items-center text-slate-500 pt-4"> {/* Use flex items-center for horizontal alignment */}
+            <Circle />
+            <span className="pl-2">Post on 22nd June 2024</span> {/* Add spacing for readability */}
           </div>
-          <div>
+          <div className="text-lg pt-5">
             {blog.content}
           </div>
         </div>
-        <div className="col-span-4">
-          <div className="text-slate-600 text-lg">
-          Author
-           <div className="w-full flex">
-         <div className="pr-2  ">  <Avatar  name={blog.author.name || "Adarsh"}/></div>
-           <div>
-           <div className="text-xl  font-bold">
-                {blog.author.name || "Adarsh"}
+        <div className="col-span-4 pt-6">
+          <div className="text-slate-600 text-xl font-semibold">
+            Author :
+            <div className="w-full flex py-2">
+              <div className="pr-2">
+                <Avatar name={blog.author.name || "Adarsh"} />
+              </div>
+              <div>
+                <div className="text-xl font-bold">
+                  {blog.author.name || "Adarsh"}
+                </div>
+                <div className="pt-2 text-slate-500">
+                  This is a detailed explanation about the user and author's ability to grab attention to users.
+                </div>
+              </div>
             </div>
-            <div className="pt-2 text-slate-500">
-                this is detail explnation about user and authro ability to grab attention to user 
-            </div>
-           </div>
-           </div>
           </div>
         </div>
       </div>
     </div>
   );
 };
+
+function Circle() {
+  return (
+    <div className="rounded-full bg-slate-700 h-2 w-2"></div>
+  );
+}
+
