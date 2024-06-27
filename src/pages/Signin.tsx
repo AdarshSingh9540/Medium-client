489ba47b81740daaedb265fb52ea36f96d9ded86
Adarsh <adarsh9540984202@gmail.com>
2024-06-27 19:21:38 +0530

    signin page update

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -3,12 +3,12 @@ import Auth from "../components/Auth";
 
 export const Signin = () => {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <div className="w-full flex justify-center items-center">
-        <div className="w-1/2 flex justify-center">
+    <div className="h-screen flex justify-center items-center"> 
+    <div className="w-full flex justify-center items-center"> 
+      <div className="w-11/12 lg:w-1/2 flex justify-center items-center">
           <Auth type="signin" />
         </div>
-        <div className="w-1/2 flex justify-center  invisible lg:visible">
+        <div className="w-1/2 justify-center hidden lg:block">
           <Quote />
         </div>
       </div>
