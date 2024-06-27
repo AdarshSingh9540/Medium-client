8fd6a69ad9c24677f869c9ae44a41b7339ac66fa
Adarsh <adarsh9540984202@gmail.com>
2024-06-27 19:19:33 +0530

    update

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -90,7 +90,7 @@ function LabelledInput({ label, placeholder, type, onChange }: LabelledInputType
   return (
     <div>
       <div>
-        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{label}</label>
+        <label className="block mb-2 text-sm font-medium text-gray-900 ">{label}</label>
         <input onChange={onChange} type={type || "text"} id="first_name"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5 "
           placeholder={placeholder} required />
